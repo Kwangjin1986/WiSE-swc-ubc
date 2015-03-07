11e2cd00bc36deec390968097a708865a2b6dfef
Anna Hippmann <ahippman@eos.ubc.ca>
2015-03-06 16:05:14 -0800

    I tried to add three files at once...

---
+++ arith.py
@@ -0,0 +1,12 @@
+import sys #allows to take arguments given in command line
+import numpy as np
+
+def main():
+    script= sys.argv[0] # at position 1 in command line after "python" 
+#it will find the 
+    num1= sys.argv[1]
+    num1= float(num1)
+    operator= sys.argv[2]
+    operator= if + then eval(operator)
+    num2= sys.argv[3]
+    num2= float(num2)
